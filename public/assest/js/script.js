25666bce18f4121312f335205d392e8d7ff9be73
Dineth Dilshan Gunawardana <infodinethdil@gmail.com>
2025-01-02 21:47:14 +0530

    #

--- public/assest/js/script.js
+++
@@ -1,8 +0,0 @@
-// Add smooth scroll for the "Explore Features" button
-document.querySelector('.cta-button').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector('#features').scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-    });
-});
