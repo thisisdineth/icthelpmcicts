dc8492985279209d8318fa89191317d5cb6c2766
Dineth Dilshan Gunawardana <infodinethdil@gmail.com>
2025-01-06 09:21:33 +0530

    Update chat.js

--- public/assest/js/chat.js
+++ public/assest/js/chat.js
@@ -1,6 +1,6 @@
 const fetchResponse = async (message) => {
     try {
-        const response = await fetch('https://icthelpmcicts-git-server-thisisdinethdnet.vercel.app/api/generate', {
+        const response = await fetch('https://icthelpmcicts-git-server-thisisdinethdnet.vercel.app/chat.html?query=hy/api/generate', {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ userQuery: message }),
